58fa11e8a615f68fd0909d8d893a8fdc15c49814
jingjing2222 <131797378+jingjing2222@users.noreply.github.com>
2023-12-19 10:36:29 +0900

    2-17

---
+++ Application Probability and Statistics/2일차/2-17.py
@@ -0,0 +1,12 @@
+import pandas as pd
+file_data = pd.read_csv("Application Probability and Statistics/생활속의통계학-파이썬-학습자용/연습문제 데이터/sample1.csv")
+print(file_data[0:5])
+
+total_score=file_data['점수']*5+file_data['출석']
+new_data=[file_data['이름'],total_score]
+
+result=pd.concat(new_data,axis=1,keys=['name','total'])
+
+print(result)
+result.to_csv("/Users/kimhyeongjeong/Desktop/학부/3-Winter/Application-Probability-and-Statistics/Application Probability and Statistics/2일차/2-17/result1.csv")
+result.to_excel("/Users/kimhyeongjeong/Desktop/학부/3-Winter/Application-Probability-and-Statistics/Application Probability and Statistics/2일차/2-17/result1.xlsx")
